180b7f475e3ef1ceb4fe6455b1d6082093ffb379
GenesisAVelizf <gvelizf@unemi.edu.ec>
2022-03-24 12:45:11 -0500

    primera parte

---
+++ backend/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// declaro una variable mensaje de tipo string, y lo presento por consola
+var mensaje = "Hola mundo desde JS";
+console.log(mensaje);
---
+++ backend/src/index.ts
@@ -0,0 +1,3 @@
+// declaro una variable mensaje de tipo string, y lo presento por consola
+var mensaje: string = "Hola mundo desde JS";
+console.log(mensaje);
