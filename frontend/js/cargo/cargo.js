1eec24dc74936a680a49d7712a7ffc5aa0d16388
GenesisAVelizf <gvelizf@unemi.edu.ec>
2022-03-28 02:12:56 -0500

    proyecto

--- frontend/js/cargo/cargo.js
+++ frontend/js/cargo/cargo.js
@@ -1,41 +1,41 @@
-// import {
-//   Cargo
-// } from "./componenteC.js";
-// // instanciamos cargo
-// const serCargo = new Cargo();
-// const d = document;
-// const $formCargo = d.getElementById("form-cargo");
-// // registrar evento
-// d.addEventListener("DOMContentLoaded", serCargo.obtenerCargos());
-// // delegacion de eventos
-// d.addEventListener("click", async (e) => {
-//   // alert("haz hecho click")
-//   console.log(e.target)
-//   if (e.target.matches("#enviar")) {
-//     e.preventDefault();
-//     let $descrip = d.getElementById("descripcion").value;
-//     let $estado = d.getElementById("activo").checked;
-//     if ($descrip.trim().length < 3) {
-//       alert("Datos vacios o incompletos");
-//     } else {
-//       if (serCargo.grabar) {
-//         // let id = Date.now();
-//         const cargo = {
-//           descripcion: $descrip,
-//           estado: $estado
-//         };
-//         const cargoJson = JSON.stringify(cargo);
-//         const res = await serCargo.insertarDatos(cargoJson);
-//       } else {
-//         let id = serCargo.id;
-//         const cargo = {
-//           descripcion: $descrip,
-//           estado: $estado
-//         };
-//         const cargoModJson = JSON.stringify(cargo);
-//         const res = await serCargo.modificarDatos(cargoModJson, id);
-//       }
-//       $formCargo.reset();
-//     }
-//   }
-// });
+import {
+    Cargo
+} from "./componenteC.js";
+// instanciamos cargo
+const serCargo = new Cargo();
+const d = document;
+const $formCargo = d.getElementById("form-cargo");
+// registrar evento
+d.addEventListener("DOMContentLoaded", serCargo.obtenerCargos());
+// delegacion de eventos
+d.addEventListener("click", async (e) => {
+    // alert("haz hecho click")
+    console.log(e.target)
+    if (e.target.matches("#enviar")) {
+        e.preventDefault();
+        let $descrip = d.getElementById("descripcion").value;
+        let $estado = d.getElementById("activo").checked;
+        if ($descrip.trim().length < 3) {
+            alert("Datos vacios o incompletos");
+        } else {
+            if (serCargo.grabar) {
+                // let id = Date.now();
+                const cargo = {
+                    descripcion: $descrip,
+                    estado: $estado
+                };
+                const cargoJson = JSON.stringify(cargo);
+                const res = await serCargo.insertarDatos(cargoJson);
+            } else {
+                let id = serCargo.id;
+                const cargo = {
+                    descripcion: $descrip,
+                    estado: $estado
+                };
+                const cargoModJson = JSON.stringify(cargo);
+                const res = await serCargo.modificarDatos(cargoModJson, id);
+            }
+            $formCargo.reset();
+        }
+    }
+});
