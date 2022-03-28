89ca618fa5d3707c8cfc6cfe999566e8c6e72bc2
GenesisAVelizf <gvelizf@unemi.edu.ec>
2022-03-27 22:41:31 -0500

    proyecto

--- frontend/js/cargo/cargo.js
+++ frontend/js/cargo/cargo.js
@@ -8,9 +8,35 @@ const $formCargo = d.getElementById("form-cargo");
 // registrar evento
 d.addEventListener("DOMContentLoaded", serCargo.obtenerCargos());
 // delegacion de eventos
-d.addEventListener("click", (e) => {
-  alert("haz hecho click")
+d.addEventListener("click", async (e) => {
+  // alert("haz hecho click")
   console.log(e.target)
+  if (e.target.matches("#enviar")) {
+    e.preventDefault();
+    let $descrip = d.getElementById("descripcion").value;
+    let $estado = d.getElementById("activo").checked;
+    if ($descrip.trim().length < 3) {
+      alert("Datos vacios o incompletos");
+    } else {
+      if (serCargo.grabar) {
+        const cargo = {
+          descripcion: $descrip,
+          estado: $estado
+        };
+        const cargoJson = JSON.stringify(cargo);
+        const res = await serCargo.insertarDatos(cargoJson);
+      } else {
+        let id = serCargo.id;
+        const cargo = {
+          descripcion: $descrip,
+          estado: $estado
+        };
+        const cargoModJson = JSON.stringify(cargo);
+        const res = await serCargo.modificarDatos(cargoModJson, id);
+      }
+      $formCargo.reset();
+    }
+  }
 });
 
 
